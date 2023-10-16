0f2a28e457776fab5750a83b2620b7832cb22ab3
Nabi Ameen <nabiameen35126@gmail.com>
2023-10-16 15:53:02 +0500

    create table

--- src/supplier/Supplier.js
+++ src/supplier/Supplier.js
@@ -3,21 +3,20 @@ import { Form, Formik } from "formik";
 import React from "react";
 
 const Supplier = () => {
+  const initialValue = {
+    supplierCategory: "",
+    supplierName: "",
+    contactNumber: "",
+    currency: "",
+    roe: "",
+    commision: "",
+    email: "",
+    address: "",
+  };
 
-    const initialValue = {
-        supplierCategory: "",
-        supplierName: "",
-        contactNumber: "",
-        currency: "",
-        roe: "",
-        commision: "",
-        email: "",
-        address: ""
-        };
-
-    const handlSubmit = (values)=>{
-        console.log("Values", values)
-    }   
+  const handlSubmit = (values) => {
+    console.log("Values", values);
+  };
 
   return (
     <>
@@ -26,7 +25,7 @@ const Supplier = () => {
       </div>
 
       <Formik initialValues={initialValue} onSubmit={handlSubmit}>
-        {({values, setFieldValue}) => (
+        {({ values, setFieldValue }) => (
           <Form>
             <div className="container mx-auto border p-5 my-5 space-y-10">
               <div className="flex">
@@ -37,7 +36,9 @@ const Supplier = () => {
                     variant="outlined"
                     name="supplierCategory"
                     value={values?.supplierCategory}
-                    onChange={(e)=> setFieldValue("supplierCategory", e.target.value)}
+                    onChange={(e) =>
+                      setFieldValue("supplierCategory", e.target.value)
+                    }
                   />
                 </div>
                 <div className="flex-1">
@@ -47,7 +48,9 @@ const Supplier = () => {
                     variant="outlined"
                     name="supplierName"
                     value={values?.supplierName}
-                    onChange={(e)=> setFieldValue("supplierName", e.target.value)}
+                    onChange={(e) =>
+                      setFieldValue("supplierName", e.target.value)
+                    }
                   />
                 </div>
                 <div className="flex-1">
@@ -57,7 +60,9 @@ const Supplier = () => {
                     variant="outlined"
                     name="contactNumber"
                     value={values?.contactNumber}
-                    onChange={(e)=> setFieldValue("contactNumber", e.target.value)}
+                    onChange={(e) =>
+                      setFieldValue("contactNumber", e.target.value)
+                    }
                   />
                 </div>
                 <div className="flex-1">
@@ -67,7 +72,7 @@ const Supplier = () => {
                     variant="outlined"
                     name="currency"
                     value={values?.currency}
-                    onChange={(e)=> setFieldValue("currency", e.target.value)}
+                    onChange={(e) => setFieldValue("currency", e.target.value)}
                   />
                 </div>
               </div>
@@ -79,7 +84,7 @@ const Supplier = () => {
                     variant="outlined"
                     name="roe"
                     value={values?.roe}
-                    onChange={(e)=> setFieldValue("roe", e.target.value)}
+                    onChange={(e) => setFieldValue("roe", e.target.value)}
                   />
                 </div>
                 <div className="flex-1">
@@ -89,7 +94,7 @@ const Supplier = () => {
                     variant="outlined"
                     name="commision"
                     value={values?.commision}
-                    onChange={(e)=> setFieldValue("commision", e.target.value)}
+                    onChange={(e) => setFieldValue("commision", e.target.value)}
                   />
                 </div>
                 <div className="flex-1">
@@ -99,7 +104,7 @@ const Supplier = () => {
                     variant="outlined"
                     name="email"
                     value={values?.email}
-                    onChange={(e)=> setFieldValue("email", e.target.value)}
+                    onChange={(e) => setFieldValue("email", e.target.value)}
                   />
                 </div>
                 <div className="flex-1">
@@ -109,18 +114,51 @@ const Supplier = () => {
                     variant="outlined"
                     name="address"
                     value={values?.address}
-                    onChange={(e)=> setFieldValue("address", e.target.value)}
+                    onChange={(e) => setFieldValue("address", e.target.value)}
                   />
                 </div>
               </div>
 
               <div className="flex justify-center">
-                <Button type="submit" variant="contained"> Save </Button>
+                <Button type="submit" variant="contained">
+                  {" "}
+                  Save{" "}
+                </Button>
               </div>
             </div>
           </Form>
         )}
       </Formik>
+
+      <div className="container mx-auto mt-20">
+        <table className="w-full">
+          <tr className="flex">
+            <th className="flex-1">Supplier Category</th>
+            <th className="flex-1">Supplier Name</th>
+            <th className="flex-1">Contact Number</th>
+            <th className="flex-1">Currency</th>
+            <th className="flex-1">ROE</th>
+            <th className="flex-1">Commision</th>
+            <th className="flex-1">Email</th>
+            <th className="flex-1">Address</th>
+            <th className="flex-1">Action</th>
+          </tr>
+          <tr className="flex">
+            <td className="flex-1">Supplier Category</td>
+            <td className="flex-1">Supplier Name</td>
+            <td className="flex-1">Contact Number</td>
+            <td className="flex-1">Currency</td>
+            <td className="flex-1">ROE</td>
+            <td className="flex-1">Commision</td>
+            <td className="flex-1">Email</td>
+            <td className="flex-1">Address</td>
+            <td className="flex-1">
+              <p className="text-green-500">Edit</p>
+              <p className="text-red-500">Delete</p>
+            </td>
+          </tr>
+        </table>
+      </div>
     </>
   );
 };
